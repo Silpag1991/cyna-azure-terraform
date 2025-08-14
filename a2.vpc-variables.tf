0317fcd662b6f1d7c48face2e9a77d59f02d75a5
Silpa Gali (Bosch Global Software Technolo) <v-silpagali@microsoft.com>
2025-08-14 16:09:41 +0530

    Vnet creation

---
+++ a2.vpc-variables.tf
@@ -0,0 +1,36 @@
+variable "vnet_name" {
+  description = "Virtual network name"
+  type = string
+  default = "vnet default"
+}
+
+variable "vnet_address_space" {
+    description = "vnet address space"
+    type = list(string)
+    default = [ "10.0.0.0/16" ]
+  
+}
+
+variable "web_subnet_name" {
+    type = string
+    default = "websubnet"
+  
+}
+
+variable "web_address_space" {
+    type = list(string)
+    default = [ "10.0.1.0/24" ]
+  
+}
+
+variable "db_subnet_name" {
+    type = string
+    default = "dbsubnet"
+  
+}
+
+variable "db_address_space" {
+    type = list(string)
+    default = [ "10.0.2.0/24" ]
+  
+}

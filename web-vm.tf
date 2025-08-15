7a7fb3126d2e4f0fe160971d0e3fbe3ee3f495e1
Silpa Gali (Bosch Global Software Technolo) <v-silpagali@microsoft.com>
2025-08-15 18:35:42 +0530

    VM added

---
+++ web-vm.tf
@@ -0,0 +1,26 @@
+resource "azurerm_linux_virtual_machine" "linuxvm" {
+ for_each = var.web_linux_instance_count
+    name                = "${local.resource_name_prefix}-nic-${each.key}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password = "Admin@123456"
+  network_interface_ids = [
+    azurerm_network_interface.web_linux_nic[each.key].id,
+  ]
+
+    os_disk {
+        name = "osdisk-${each.key}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  custom_data = filebase64("${path.module}/app/app.sh")
+}
